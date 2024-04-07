75644a9121571d931a05a32cae0ba693ece9e461
Tartakovskyi Artem <artem.tartakovskiy@gmail.com>
2024-04-07 22:27:42 +0300

    Added a login.html template

--- djangochat/core/urls.py
+++ djangochat/core/urls.py
@@ -5,6 +5,7 @@ URL Patterns:
 - "" (empty string): Maps to the frontpage view
 - "signup/": Maps to the signup view
 """
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -12,4 +13,6 @@ from . import views
 urlpatterns = [
     path("", views.frontpage, name="frontpage"),
     path("signup/", views.signup, name="signup"),
+    path("login/", auth_views.LoginView.as_view(template_name="core/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]

4159482cdcd398aaf353a48646fde8dc49a3ac8c
Tartakovskyi Artem <artem.tartakovskiy@gmail.com>
2024-04-08 21:47:43 +0300

    Added a room.html template and updated the Room model

--- djangochat/room/models.py
+++ djangochat/room/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Room(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True, null=True)

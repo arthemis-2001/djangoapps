de3d560d48a72ec7755e4eb32d5a3c8658971b61
Tartakovskyi Artem <artem.tartakovskiy@gmail.com>
2024-04-09 16:57:28 +0300

    Added new models and urls for the portfolio project

--- portfolio/myportfolio/models.py
+++ portfolio/myportfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class PortfolioItem(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to="portfolio_images/")
+    url = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -78,6 +78,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        "PORT": "5400",
     }
 }
 
@@ -117,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

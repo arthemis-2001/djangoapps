4ead08457cdabbcae170b06b05eb8b3307e08c5b
Tartakovskyi Artem <artem.tartakovskiy@gmail.com>
2024-04-10 20:36:13 +0300

    Created a portfolio_detail web page

--- portfolio/myportfolio/models.py
+++ portfolio/myportfolio/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class PortfolioItem(models.Model):
@@ -8,4 +9,9 @@ class PortfolioItem(models.Model):
     url = models.URLField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    
+
+class Comment(models.Model):
+    portfolio_item = models.ForeignKey(PortfolioItem, on_delete=models.CASCADE, related_name = 'comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
--- portfolio/myportfolio/urls.py
+++ portfolio/myportfolio/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.portfolio_list, name="portfolio_list"),
+    path("portfolio/<int:pk>/", views.portfolio_detail, name="portfolio_detail"),
 ]
 

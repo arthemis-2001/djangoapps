b3ce1ede96e6d0c04e9e1807feb117e763e7cf29
Tartakovskyi Artem <artem.tartakovskiy@gmail.com>
2024-04-10 19:27:58 +0300

    Created three PortfolioItems

--- portfolio/myportfolio/admin.py
+++ portfolio/myportfolio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import PortfolioItem
 
 # Register your models here.
+@admin.register(PortfolioItem)
+class PortfolioItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'image', 'url', 'created_at')
+
--- portfolio/myportfolio/views.py
+++ portfolio/myportfolio/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import PortfolioItem
 
 # Create your views here.
+def portfolio_list(request):
+    items = PortfolioItem.objects.all()
+    return render(request, "myportfolio/portfolio_list.html", {"items": items})

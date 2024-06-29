84e58be10dbdccbf11b2e483deceb6b50bfab90f
Tartakovskyi Artem <artem.tartakovskiy@gmail.com>
2024-06-29 20:01:09 +0200

    Fixed some errors, e.g. toggling the hamburger menu

--- portfolio/myportfolio/urls.py
+++ portfolio/myportfolio/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.portfolio_list, name="portfolio_list"),
     path("portfolio/<int:pk>/", views.portfolio_detail, name="portfolio_detail"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
     path("register/", views.register, name="register"),
     path("portfolio/<int:pk>/like/", views.portfolio_like, name="portfolio_like")
 ]
